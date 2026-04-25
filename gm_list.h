73db6b9578fdd22a50aab422b0867a72bd8fed60
Usser65-87-11 <googa.vo1d@gmail.com>
2026-04-26 00:31:53 +0300

    up

---
+++ gm_list.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdint.h>
+#include "gm_array2.h"
+
+ struct GmNode {
+    void* data;
+    struct GmNode* next;
+} ;
+
+ struct GmList{
+   struct GmNode* head;
+   struct GmNode* tail;
+    uint32_t size;
+} ;
+
+void gmListInit(struct GmList* list) ;
+bool gmListPushBack(struct GmList* list, void* data);
+
+bool gmListPushFront(struct GmList* list, void* data);
+void * gmListPopFront(struct GmList* list);
+void gmListForeach(struct GmList* list, void (*fn)(void*));
+void gmListFree(struct GmList* list);
+
+bool gmListToGmArray(struct GmList* list, struct GmArray * out,uint32_t width,uint32_t alignment);
