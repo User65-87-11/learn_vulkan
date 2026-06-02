264166c4c087e2d91d30f58c71a25cc6653e5f08
Usser65-87-11 <googa.vo1d@gmail.com>
2026-06-02 18:00:57 +0300

    up

--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define DEFAULT_SHADER_VERT "shaders/out/vert.spv"
-#define DEFAULT_SHADER_FRAG "shaders/out/frag.spv"
+#define DEFAULT_SHADER_VERT "shaders/out/main.vert.spv"
+#define DEFAULT_SHADER_FRAG "shaders/out/main.frag.spv"
